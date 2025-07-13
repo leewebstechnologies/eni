79a118598536a71bcc9466d0c6510f96ce4be7b0
Ali <aliuajibade317@gmail.com>
2025-07-13 09:22:07 +0100

    create header section

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Eni</title>
+  <link rel="stylesheet" href="./css/styles.css"/>
+</head>
+<body>
+  <header>
+    <img src="./images/logo.png" alt="logo" class="logo">
+    <nav>
+      <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">Content Management</a></li>
+        <li><a href="">Interactive Tools</a></li>
+        <li><a href="">Feedback</a></li>
+      </ul>
+    </nav>
+    <div class="search-box">
+      <input type="text" placeholder="Search...">
+      <button>Search</button>
+    </div>
+  </header>
+</body>
+</html>
